0a4fa02fd9ea445284aa57111a181b8b82cf7d37
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2024-02-29 17:30:02 +0100

    Added filtering

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,31 @@ const RECOMMENDEDURL = "/assets/data/recommended.json";
 const BRANDSURL = "/assets/data/brands.json";
 const SWITCHTYPESURL = "/assets/data/switchtypes.json";
 const PRODUCTSURL = "/assets/data/products.json";
+const SORTVALUESURL = "/assets/data/sorttypes.json";
+const CONNECTIVITYURL = "/assets/data/connectivity.json";
+const SPECIFICATIONSURL = "/assets/data/specifications.json";
+const SIZESURL = "/assets/data/sizes.json";
+const PRODUCTTYPESURL = "/assets/data/producttypes.json";
 
+const PRODUCTS = [];
 const BRANDS = [];
 const SWITCHTYPES = [];
-const PRODUCTS = [];
+const SORTVALUES = [];
+const CONNECTIVITY = [];
+const SPECIFICATIONS = [];
+const SIZES = [];
+const PRODUCTTYPES = [];
+
+const PRODUCTSBLOCK = "#products";
+
+const FILTER = {
+    "brand" : [],
+    "switch" : [],
+    "connectivity" : [],
+    "specification" : [],
+    "size" : [],
+    "producttype" : []
+}
 
 // Used to fetch a single json file
 async function fetchData(url) {
@@ -33,16 +54,29 @@ async function initializePage() {
 
     if (PATH == "index.html") {
         let recommended = await fetchData(RECOMMENDEDURL);
-        let recommendedBlock = $("#recommended");
-        displayProducts(recommendedBlock, recommended);
+        displayProducts(recommended);
     }
 
     if (PATH == "products.html") {
         let products = await fetchData(PRODUCTSURL);
         products.forEach(p => PRODUCTS.push(p));
+        displayProducts(products);
 
-        let productsBlock = $("#all-products");
-        displayProducts(productsBlock, products);
+        let sortValues = await fetchData(SORTVALUESURL);
+        sortValues.forEach(s => SORTVALUES.push(s));
+        let sortBlock = $("#sort");
+        showSortTypes(sortBlock, SORTVALUES);
+
+        let brandFilterBlock = $("#brand");
+        showFilter(brandFilterBlock, BRANDS);
+
+        let switchFilterBlock = $("#switch");
+        showFilter(switchFilterBlock, SWITCHTYPES);
+
+        await generateFilter(PRODUCTTYPESURL, PRODUCTTYPES, "producttype");
+        await generateFilter(CONNECTIVITYURL, CONNECTIVITY, "connectivity");
+        await generateFilter(SPECIFICATIONSURL, SPECIFICATIONS, "specification");
+        await generateFilter(SIZESURL, SIZES, "size");
     }
 }
 
@@ -64,19 +98,41 @@ function displayNavigation(navArray) {
     }
 }
 
-function displayProducts(displayBlock, productsArray) {
+function displayProducts(productsArray) {
+    // Filtering products to display
+    let arr = productsArray;
+    if (!isFilterEmpty()) {
+        console.log("FIL")
+        for (const f in FILTER) {
+            if (FILTER[f].length != 0) {
+                let filterName = "";
+                switch (f) {
+                    case "brand" : filterName = "brand_id"; break;
+                    case "switch" : filterName = "switch_types"; break;
+                    case "connectivity" : filterName = "connectivity"; break;
+                    case "specification" : filterName = "specifications"; break;
+                    case "size" : filterName = "size"; break;
+                    case "producttype" : filterName = "product_type"; break;
+                }
+                console.log(FILTER[f])
+                arr = applyFilter(FILTER[f], arr, filterName);
+            }
+        }
+    }
     // Calculating rows and adding product placeholders
     let html = "";
-    let rowCount = Math.ceil(productsArray.length / 4);
+    console.log(arr);
+    let rowCount = Math.ceil(arr.length / 4);
     for (let i = 0; i < rowCount * 4; i++) {
         html += '<div class="mm-product mb-4 mx-auto"></div>';
     }
-    $(displayBlock).html(html);
+    $(PRODUCTSBLOCK).html(html);
+    console.log($(PRODUCTSBLOCK))
 
     // Putting products in placeholders
     let i = 0;
     let productHolders = $(".mm-product");
-    for (const product of productsArray) {
+    for (const product of arr) {
         $(productHolders[i++]).html(`
             <div class="p-3 card position-relative h-100">
                 <img class="card-img-top" src="${product.img.src}" alt="${product.img.alt}"/>
@@ -91,6 +147,79 @@ function displayProducts(displayBlock, productsArray) {
             </div>
         `);
     }
+}
+
+function applyFilter(validValues, arrayToFilter, propertyToFilter) {
+    // Fitering out all products that dont have the right property
+    let arr = [];
+    for (const element of arrayToFilter) {
+        if (element.hasOwnProperty(propertyToFilter)) {
+            arr.push(element);
+        }
+    }
+
+    if (Array.isArray(arr[0][propertyToFilter])) {
+        return arr.filter(x => {
+            console.log(x);
+            for (const value of x[propertyToFilter]) {
+                if (validValues.includes(value)) return true;
+            }
+            return false;
+        });
+    }
+    return arr.filter(x => validValues.includes(x[propertyToFilter]));
+}
+
+function showSortTypes(displayBlock, sortArray) {
+    let html = "";
+    for (const sortOption of sortArray) {
+        html += `
+            <option value="${sortOption.id}">${sortOption.name}</option>
+        `;
+    }
+    $(displayBlock).html(html);
+}
+
+async function generateFilter(filterURL, filterARR, displayBlockId) {
+    let filterVals = await fetchData(filterURL);
+    filterVals.forEach(f => filterARR.push(f));
+    let filterDisplayBlock = $(`#${displayBlockId}`);
+    showFilter(filterDisplayBlock, filterARR);
+}
+
+function showFilter(displayBlock, filterArray) {
+    let filterName = $(displayBlock).attr("id");
+    let html = "";
+    for (const filterOption of filterArray) {
+        html += `
+            <li class="list-group-item">
+                <input class="form-check-input me-1" type="checkbox" onChange="updateFilters('${filterName}',${filterOption.id})" id="${filterName}-${filterOption.id}">
+                <label class="form-check-label" for="${filterName}-${filterOption.id}">${filterOption.name}</label>
+            </li>
+        `;
+    }
+    $(displayBlock).html(html);
+}
+
+function updateFilters(filterName, filterValue) {
+    console.log(filterName);
+    console.log(filterValue);
+    if (FILTER[filterName].includes(filterValue)) {
+        let i = FILTER[filterName].indexOf(filterValue);
+        FILTER[filterName] = FILTER[filterName].filter(x => x != filterValue)
+    } else {
+        FILTER[filterName].push(filterValue);
+    }
+    displayProducts(PRODUCTS);
+}
+
+function isFilterEmpty() {
+    console.log(FILTER)
+    console.log(Object.keys(FILTER))
+    for (const f of Object.keys(FILTER)) {
+        if (FILTER[f].length != 0) return false;
+    }
+    return true;
 }
 
 function getBrandName(id) {
